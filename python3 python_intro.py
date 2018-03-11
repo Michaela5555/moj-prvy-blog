72ca8c60fce18c56e2ae0e46dac066d2b9e31e55
Miska <okno@okno.sk>
2018-03-11 15:11:43 +0100

    My Django Girls app, first commit

---
+++ python3 python_intro.py
@@ -0,0 +1,8 @@
+print("Hello, Django girls!")
+if 3>2:
+	print("It works !")
+if 5>2:
+	print("5 je vacsie ako 2")
+else:
+	print("5 nie je vacsie ako 2")
+	
---
+++ test.py
@@ -0,0 +1,7 @@
+def hi(meno):
+	print("Ahoj" + meno + "!")
+		
+dievcata = ["Miška", "Katka", "Alex"]
+for meno in dievcata:
+	hi(meno)
+	print("dalsie dievcata")
